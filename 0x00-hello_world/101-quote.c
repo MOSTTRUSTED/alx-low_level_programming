78bd6cd469bf302f6743948c1245b71d3b7a72b5
Abiodun Adewale <adewaleabidounade@gmail.com>
2023-10-05 13:28:17 -0700

    C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
-	write(2, message, strlen(message));
+	char *sag = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, sag, strlen(message));
 	return (1);
 }
