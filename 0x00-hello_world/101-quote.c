05cfd6a2a56455a03d3d5f75d50edd47514904cc
Abiodun Adewale <adewaleabidounade@gmail.com>
2023-10-05 13:12:53 -0700

    C

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+	return (1);
+}
